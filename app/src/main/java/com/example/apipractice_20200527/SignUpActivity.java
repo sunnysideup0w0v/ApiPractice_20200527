1293993a5de541ff3095c240e35544f4732d5f1d
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-28 10:15:38 +0900

    비밀번호 관련 검사를 별개의 메소드로 분리  => 비밀벌호 입력/ 확인 모두 같은 메소드 내에서 검사 실행

--- app/src/main/java/com/example/apipractice_20200527/SignUpActivity.java
+++ app/src/main/java/com/example/apipractice_20200527/SignUpActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 import com.example.apipractice_20200527.databinding.ActivitySignUpBinding;
 
@@ -19,9 +22,58 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.pwEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkPasswords();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.pwRepeatEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkPasswords();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
-
+//   응용문제 => 비번/ 비번확인 중 어느것을 타이핑해도 매번 둘 다 검사하게끔.
+    void checkPasswords() {
+        String pw = binding.pwEdt.getText().toString();
+        if(pw.length() == 0){
+            binding.pwCheckResultTxt.setText("비밀번호를 입력해주세요");
+        } else if(pw.length()<8){
+            binding.pwCheckResultTxt.setText("비밀번호가 너무 짧습니다");
+        } else {
+            binding.pwCheckResultTxt.setText("사용해도 좋은 비밀번호입니다");
+        }
+        String pwRepeat = binding.pwRepeatEdt.getText().toString();
+        if(pwRepeat.length()==0){
+            binding.pwRepeatCheckResultTxt.setText("비밀번호를 입력해주세요");
+        } else if(pwRepeat.equals(pw)){
+            binding.pwRepeatCheckResultTxt.setText("비밀번호가 서로 일치합니다");
+        } else {
+            binding.pwRepeatCheckResultTxt.setText("비밀번호가 서로 일치하지 않습니다");
+        }
+    }
     @Override
     public void setValues() {
 
