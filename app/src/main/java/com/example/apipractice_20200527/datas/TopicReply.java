07c413b9cba80f0be21197ff873358484b732983
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-29 16:46:46 +0900

    댓글의 작성자 정보 파싱  - User클래스에 만들어둔 파싱 기능 재활용

--- app/src/main/java/com/example/apipractice_20200527/datas/TopicReply.java
+++ app/src/main/java/com/example/apipractice_20200527/datas/TopicReply.java
@@ -7,20 +7,27 @@ public class TopicReply {
     private int id;
     private String content;
     private String side;
+    private User writer;
+
 
 
     public static TopicReply getTopicReplyFromJson(JSONObject jsonObject){
-        TopicReply tp = new TopicReply();
+        TopicReply tr = new TopicReply();
         try {
-            tp.id = jsonObject.getInt("id");
-            tp.content = jsonObject.getString("content");
-            tp.content = jsonObject.getString("side");
+            tr.id = jsonObject.getInt("id");
+            tr.content = jsonObject.getString("content");
+            tr.side = jsonObject.getString("side");
+//댓글 제이슨 안에 있는 user json 추출
+            JSONObject user = jsonObject.getJSONObject("user");
+//
+            tr.writer = User.getUserFromJson(user);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
 
-        return tp;
+        return tr;
     }
 
     public TopicReply() {
@@ -54,5 +61,16 @@ public class TopicReply {
 
     public void setSide(String side) {
         this.side = side;
+    }
+    public User getWriter() {
+        return writer;
+    }
+
+    public void setWriter(User writer) {
+        this.writer = writer;
+    }
+
+    public TopicReply(User writer) {
+        this.writer = writer;
     }
 }
